7e13d46cf62d1f75a4ba549bca8845756ed94409
Anthony Tran <58158915+anthonyscry@users.noreply.github.com>
2026-01-14 17:22:15 -0800

    Restore legacy scripts entry points

---
+++ scripts/Utilities/Start-AppLockerMonitor.ps1
@@ -0,0 +1,8 @@
+#Requires -Version 5.1
+
+$target = Join-Path $PSScriptRoot '..' '..' 'src' 'Utilities' 'Start-AppLockerMonitor.ps1'
+if (-not (Test-Path $target)) {
+    throw "Target script not found: $target"
+}
+
+& $target @args
